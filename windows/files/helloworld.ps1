c01b6ea960a25abdab0fc276e251fa2f5cc3c023
John Edrington <jedrington+git@redhat.com>
2017-03-08 14:36:14 -0500

    Update helloworld.ps1

--- windows/files/helloworld.ps1
+++ windows/files/helloworld.ps1
@@ -1,17 +1,16 @@
 # Filename: helloworld.ps1
 	# This PowerShell code for Windows was generated using the WMI Code Generator, Version 10.0 RC3
 	# http://www.robvanderwoude.com/wmigen.php
-
-	$instances = Get-WMIObject -Class "Win32_DisplayConfiguration" -Namespace "root/CIMV2" -Computername $computer
-
-	foreach ( $item in $instances ) {
-		Write-Host "DeviceName       : " $item.DeviceName
-		Write-Host "PelsWidth        : " $item.PelsWidth
-		Write-Host "PelsHeight       : " $item.PelsHeight
-		Write-Host "BitsPerPel       : " $item.BitsPerPel
-		Write-Host "DisplayFrequency : " $item.DisplayFrequency
-		Write-Host
-	}
+ -param( [string]$computer = "." ) 
+$instances = Get-WMIObject -Class "Win32_DisplayConfiguration" -Namespace "root/CIMV2" -Computername $computer
+foreach ( $item in $instances ) {
+	Write-Host "DeviceName       : " $item.DeviceName
+	Write-Host "PelsWidth        : " $item.PelsWidth
+	Write-Host "PelsHeight       : " $item.PelsHeight
+	Write-Host "BitsPerPel       : " $item.BitsPerPel
+	Write-Host "DisplayFrequency : " $item.DisplayFrequency
+	Write-Host
+}
 
 msg * "Your message goes here"
 
