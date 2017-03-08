06232a7f3432fa97d138a4c71dcc6a2193526173
John Edrington <jedrington+git@redhat.com>
2017-03-08 14:43:43 -0500

    Update helloworld.ps1

--- windows/files/helloworld.ps1
+++ windows/files/helloworld.ps1
@@ -1,17 +1,73 @@
 # Filename: helloworld.ps1
-	# This PowerShell code for Windows was generated using the WMI Code Generator, Version 10.0 RC3
-	# http://www.robvanderwoude.com/wmigen.php
- -param( [string]$computer = "." ) 
-$instances = Get-WMIObject -Class "Win32_DisplayConfiguration" -Namespace "root/CIMV2" -Computername $computer
-foreach ( $item in $instances ) {
-	Write-Host "DeviceName       : " $item.DeviceName
-	Write-Host "PelsWidth        : " $item.PelsWidth
-	Write-Host "PelsHeight       : " $item.PelsHeight
-	Write-Host "BitsPerPel       : " $item.BitsPerPel
-	Write-Host "DisplayFrequency : " $item.DisplayFrequency
-	Write-Host
-}
-
+Write-Host
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Send-NetMessage 'Hello all!' 
 msg * "Your message goes here"
 
+# Remote computer argument for Windows only, not for Linux
+
+param( [string]$computer = "." )
+
+ 
+
+if ( $HOME[0] -eq "/" ) {
+
+	# Linux
+
+	# Command line argument is ignored.
+
+	# Less info than Windows' WMI version.
+
+ 
+
+	# xrandr/Select-String tip by diegows on StackOverflow.com
+
+	# http://superuser.com/a/418700
+
+	$info = ( xrandr | Select-String -Pattern "\*" ) -split "\s+"
+
+ 
+
+	Write-Host "DeviceName       : "
+
+	Write-Host "PelsWidth        : " ( $info[1] -split "x" )[0]
+
+	Write-Host "PelsHeight       : " ( $info[1] -split "x" )[1]
+
+	Write-Host "BitsPerPel       : "
+
+	Write-Host "DisplayFrequency : " ( $info[2] -split "\*" )[0]
+
+	Write-Host
+
+} else {
+
+	# This PowerShell code for Windows was generated using the WMI Code Generator, Version 10.0 RC3
+
+	# http://www.robvanderwoude.com/wmigen.php
+
+ 
+
+	$instances = Get-WMIObject -Class "Win32_DisplayConfiguration" -Namespace "root/CIMV2" -Computername $computer
+
+ 
+
+	foreach ( $item in $instances ) {
+
+		Write-Host "DeviceName       : " $item.DeviceName
+
+		Write-Host "PelsWidth        : " $item.PelsWidth
+
+		Write-Host "PelsHeight       : " $item.PelsHeight
+
+		Write-Host "BitsPerPel       : " $item.BitsPerPel
+
+		Write-Host "DisplayFrequency : " $item.DisplayFrequency
+
+		Write-Host
+
+	}
+
+}
 # end of script
